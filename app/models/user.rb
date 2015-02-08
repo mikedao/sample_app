47581db43e565c38bb9f496ff1a164f021fa2d99
Michael Dao <michael.dao@gmail.com>
2015-02-08 12:32:52 -0700

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,10 @@ class User < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
-
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                        BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-  end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
 
   def remember
     self.remember_token = User.new_token
@@ -31,6 +21,19 @@ class User < ActiveRecord::Base
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+  class << self
+
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
   end
 
 end
